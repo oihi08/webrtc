811e427ff7dec38b9346304c2fb32d2e2a37d16c
Oihi <oihi@tienda-pc5.dominiontibilb.intra>
2015-02-18 09:10:02 +0100

    Server added

---
+++ server.js
@@ -0,0 +1,10 @@
+var static = require('node-static');
+
+var file = new static.Server();
+
+require('http').createServer(function (request, response) {
+    request.addListener('end', function () {
+        file.serve(request, response);
+    }).resume();
+}).listen(8080);
+console.log("localhost:8080");
